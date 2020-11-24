d4d4d21109f78459d7111f62016ef0ec9d5dbbd3
Xavianna <madelainelauw@gmail.com>
2020-11-24 18:13:44 +0000

    Update Task-3.1.6.js
    Created bill calculations.

---
+++ Module-3/scripts/Task-3.1.6.js
@@ -0,0 +1,19 @@
+var mealPrice = 40.50;
+var tipAmt = (mealPrice* 12.5) / 100;
+var totalMealPrice = mealPrice + tipAmt;
+
+alert('For your meal today, you have given a tip of £'+tipAmt+'.');
+
+document.write('Your total bill, with tip, is £'+totalMealPrice+'.');
+
+console.log('Your total bill, with tip, is £'+totalMealPrice+'.');
+
+
+
+
+
+
+
+// var mealPrice = 40;
+// var tipAmt = (40 * 10) / 100;
+// var totalMealPrice = mealPrice + tipAmt;
