ff9c028d117fff4ed16866b344da2989566b933a
Xavianna <madelainelauw@gmail.com>
2020-11-24 18:14:21 +0000

    Update Task-3.1.6.js
    Added toFixed(2) rounding up to 2 decimal points.

--- Module-3/scripts/Task-3.1.6.js
+++ Module-3/scripts/Task-3.1.6.js
@@ -2,13 +2,20 @@ var mealPrice = 40.50;
 var tipAmt = (mealPrice* 12.5) / 100;
 var totalMealPrice = mealPrice + tipAmt;
 
-alert('For your meal today, you have given a tip of £'+tipAmt+'.');
+var tipAmtRound = tipAmt.toFixed(2); //Round amount up to 2 decimal points.
+var totalMealPriceRound = totalMealPrice.toFixed(2); //Round amount up to 2 decimal points.
 
-document.write('Your total bill, with tip, is £'+totalMealPrice+'.');
+// alert('For your meal today, you have given a tip of £'+tipAmt+'.');
 
-console.log('Your total bill, with tip, is £'+totalMealPrice+'.');
+// document.write('Your total bill, with tip, is £'+totalMealPrice+'.');
 
+// console.log('Your total bill, with tip, is £'+totalMealPrice+'.');
 
+alert('For your meal today, you have given a tip of £'+tipAmtRound+'.');
+
+document.write('Your total bill, with tip, is £'+totalMealPriceRound+'.');
+
+console.log('Your total bill, with tip, is £'+totalMealPriceRound+'.');
 
 
 
