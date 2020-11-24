fc36cef1c500f3f9e0f44788a65b948645e5e623
Xavianna <madelainelauw@gmail.com>
2020-11-24 17:31:43 +0000

    Update Task-3.1.4.js
    Added 2 variables and did the addition Arithmetic Operators.

---
+++ Module-3/scripts/Task-3.1.4.js
@@ -0,0 +1,8 @@
+var myAge = 35;
+var partnerAge = 46;
+
+alert('The total of our age is ' + (myAge + partnerAge));
+
+console.log('The total of our age is ' + (myAge + partnerAge));
+
+document.write('The total of our age is ' + (myAge + partnerAge));
