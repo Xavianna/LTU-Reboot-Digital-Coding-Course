020b0fcf853d6470bd5615607e04eb9c2c62a91d
Xavianna <madelainelauw@gmail.com>
2020-11-24 17:33:02 +0000

    Update Task-3.1.4.js
    Added a total variable to make it simpler.

--- Module-3/scripts/Task-3.1.4.js
+++ Module-3/scripts/Task-3.1.4.js
@@ -1,8 +1,15 @@
 var myAge = 35;
 var partnerAge = 46;
+var totalAge = myAge + partnerAge;
 
-alert('The total of our age is ' + (myAge + partnerAge));
+// alert('The total of our age is ' + (myAge + partnerAge));
 
-console.log('The total of our age is ' + (myAge + partnerAge));
+// console.log('The total of our age is ' + (myAge + partnerAge));
 
-document.write('The total of our age is ' + (myAge + partnerAge));
+// document.write('The total of our age is ' + (myAge + partnerAge));
+
+alert('The total of our age is ' + totalAge);
+
+console.log('The total of our age is ' + totalAge);
+
+document.write('The total of our age is ' + totalAge);
