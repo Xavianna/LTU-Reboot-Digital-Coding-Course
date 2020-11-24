a6a78eccbb8476c8d05fb4078a4d1b21e833a08e
Xavianna <madelainelauw@gmail.com>
2020-11-24 17:26:23 +0000

    Task 3.1.3

---
+++ Module-3/scripts/Task-3.1.3.js
@@ -0,0 +1,7 @@
+var numberOfKittens = 5;
+
+alert('There are ' + numberOfKittens + ' kittens');
+
+console.log('There are ' + numberOfKittens + ' kittens');
+
+document.write('There are ' + numberOfKittens + ' kittens');
