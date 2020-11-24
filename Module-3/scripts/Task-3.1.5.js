a5b0a60c2b8749c427370d1a7e65b1f7a9b7fc01
Xavianna <madelainelauw@gmail.com>
2020-11-24 17:46:50 +0000

    Update Task-3.1.5.js
    Used the concatenate operator.

---
+++ Module-3/scripts/Task-3.1.5.js
@@ -0,0 +1,9 @@
+var firstName = 'Madelaine ';
+var fullName = firstName + 'Lauw';
+fullName += ' (Xavianna)' //Added my nickname using the concatenate operator.
+
+alert(fullName) //Pops Up 'Madelaine Lauw'
+
+console.log(fullName) //Outputs 'Madelaine Lauw' in the console
+
+document.write(fullName); //Outputs 'Madelaine Lauw' in the browser
