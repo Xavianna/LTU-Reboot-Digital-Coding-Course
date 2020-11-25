8873f75364532f0d9aab6eb7e085ab2e42ce1aaf
Xavianna <madelainelauw@gmail.com>
2020-11-25 14:14:05 +0000

    Update Task-3.3.1.js

---
+++ Module-3/scripts/Task-3.3.1.js
@@ -0,0 +1,10 @@
+function percentageCalculator (number, percentage)
+{
+    var result = (number * percentage) / 100;
+
+    return result;
+}
+
+console.log ( percentageCalculator(135, 30) );
+
+console.log ( percentageCalculator(207, 12.5) );
