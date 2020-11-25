50a9646163f90bd3e44eba3dca21cfa62de8ee16
Xavianna <madelainelauw@gmail.com>
2020-11-25 11:14:24 +0000

    Add Scope example
    Global & Local Scopes.

--- Module-3/scripts/scope.js
+++ Module-3/scripts/scope.js
@@ -4,4 +4,29 @@
 
 var globalScope = 'GLOBAL SCOPE'; //Available for anything in our project
 
+var localScope = 'GLOBALLY (LocalScope)';
+
 console.log( globalScope );
+
+//A function to call globalScope in the localScope
+function scope(){
+    
+    var localScope = 'LOCALSCOPE';
+
+    console.log ( globalScope );
+
+    return localScope;
+
+}
+
+// console.log( localScope );
+
+// scope(); // To be able to access both scopes.
+
+var scopeValue = scope(); //A variable to display all the scopes
+
+console.log ( scopeValue );
+
+
+console.log(globalScope);
+console.log(localScope);
