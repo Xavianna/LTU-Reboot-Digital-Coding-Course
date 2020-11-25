f51f63407d431a0595110f58589b75d99a1c7798
Xavianna <madelainelauw@gmail.com>
2020-11-25 13:25:24 +0000

    If statement.

---
+++ Module-3/scripts/Task-3.2.4.js
@@ -0,0 +1,8 @@
+var temperature = 40; //default value was 50
+
+if(temperature<50) {
+
+    console.log('Please put on your coat!');
+
+    console.log('The temperature is ' + temperature + ' degrees. It is cold, please put a jacket on!');
+}
