8ffa15df5c623b6896c8e26db8c5cf7505c18aef
Xavianna <madelainelauw@gmail.com>
2020-11-25 13:48:52 +0000

    Else If Statement

---
+++ Module-3/scripts/Task-3.2.5.js
@@ -0,0 +1,31 @@
+// var temperature = 40; //default value was 50
+
+// if(temperature<50) {
+
+//     console.log('Please put on your coat!');
+// }
+
+// else{
+//     console.log('You can put on your summer clothes.');
+// }
+
+
+var temperature = -2; //default value was 50
+
+if(temperature < 0) {
+
+    console.log('Stay Inside!');
+
+} else if (temperature < 30){
+
+    console.log('Wear hat and coat!');
+
+} else if (temperature < 50){
+
+    console.log('Wear a coat')
+
+}else{
+
+    console.log('No need to wear your hats and coat, its lovely weather outside.');
+
+}
