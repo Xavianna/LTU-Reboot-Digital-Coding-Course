efc5a8193d36857844bfe97562510a02868f7b6f
Xavianna <madelainelauw@gmail.com>
2020-11-25 15:49:31 +0000

    Updated task 3.2.6 with logical operators.

--- Module-3/scripts/Task-3.2.6.js
+++ Module-3/scripts/Task-3.2.6.js
@@ -1,22 +1,43 @@
 //Shall I wear a coat program.
 
-var temperature = 40; //default value was 50
+// var temperature = 40; //default value was 50
 
-if(temperature < 0) {
+// if(temperature < 0) {
+
+//     console.log('Stay Inside!');
+
+// } else if (temperature < 30 || temperature <50){
+
+//     console.log('Wear hat and coat!');
+
+// } else{
+
+//     console.log('No need to wear your hats and coat, its lovely weather outside.');
+// }
+
+//     // else if (temperature != 50){
+
+//     //     console.log('Wear a coat')
+
+//     // }
+
+var temperature = -2; //default value was 50
+
+var snowing = true;
+
+if(temperature < 0 && snowing ) {
 
     console.log('Stay Inside!');
 
-} else if (temperature < 30 || temperature <50){
+} else if (temperature < 30){
 
     console.log('Wear hat and coat!');
 
-} else{
+} else if (temperature < 50){
+
+    console.log('Wear a coat')
+
+}else{
 
     console.log('No need to wear your hats and coat, its lovely weather outside.');
 }
-
-    // else if (temperature != 50){
-
-    //     console.log('Wear a coat')
-
-    // }
