2240775e139f4ed40739c7ac594a872298306ab5
Xavianna <madelainelauw@gmail.com>
2020-11-25 10:50:22 +0000

    Task 3.2.3
    Different functions - using return and console.log to display name.

---
+++ Module-3/scripts/Task-3.2.3.js
@@ -0,0 +1,9 @@
+function fullName (firstName, lastName){
+    return firstName + ' ' + lastName ;
+}
+
+console.log( fullName('Madelaine', ' Lauw') );
+
+var nameOfPerson = fullName ('Wayne', 'Heginbotham');
+
+console.log ( nameOfPerson );
