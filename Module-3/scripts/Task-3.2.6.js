f3c861da7d18c1fae7151661d4790a88524b0b38
Xavianna <madelainelauw@gmail.com>
2020-11-25 14:01:43 +0000

    Update Task-3.2.6.js

---
+++ Module-3/scripts/Task-3.2.6.js
@@ -0,0 +1,23 @@
+//Shall I wear a coat program.
+
+var temperature = 40; //default value was 50
+
+if(temperature < 0) {
+
+    console.log('Stay Inside!');
+
+} else if (temperature < 30 || temperature <50){
+
+    console.log('Wear hat and coat!');
+
+} else{
+
+    console.log('No need to wear your hats and coat, its lovely weather outside.');
+
+}
+
+    // else if (temperature != 50){
+
+    //     console.log('Wear a coat')
+
+    // }
