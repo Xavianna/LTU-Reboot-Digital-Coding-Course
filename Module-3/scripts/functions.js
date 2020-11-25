86150af2a4c2118eebd33e9fac6d8af8bb271c55
Xavianna <madelainelauw@gmail.com>
2020-11-25 09:52:45 +0000

    functions

---
+++ Module-3/scripts/functions.js
@@ -0,0 +1,17 @@
+/*
+Declaring a function
+
+To declare a function, you use the function keyword, followed by the function name, a list of parameters, and the function body.
+
+function functionName( parameters ) {
+    
+    function body
+
+    CODE GOES HERE
+}
+*/
+
+function greeting( name ){
+
+    console.log(name);
+}
