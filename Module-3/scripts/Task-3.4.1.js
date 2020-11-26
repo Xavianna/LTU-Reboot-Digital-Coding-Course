d549b5b4ac380bb8b07e09b258fecd335455a539
Xavianna <madelainelauw@gmail.com>
2020-11-26 10:18:54 +0000

    Times table task using for loops

---
+++ Module-3/scripts/Task-3.4.1.js
@@ -0,0 +1,24 @@
+//For Loop will count 1 to 10
+
+for (var i=1; i<=10; i++){
+    var mul= 7*i;
+    console.log (mul)
+}
+
+
+//Example from Luke
+for (var index = 1; index <= 12; index++){
+    var result = index * 7;
+
+    console.log('7' + 'x' + index + '=' + result);
+}
+
+
+//Example from Luke do all times table [Bonus]
+for (var amount = 1; amount <= 12; amount++){
+    
+    var number = 9;
+    var result = amount * number;
+
+    console.log(number + 'x' + amount + '=' + result);
+}
