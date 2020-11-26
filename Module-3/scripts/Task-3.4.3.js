7f8bc60a8a0d15967535bf9c369a40ff72b048f1
Xavianna <madelainelauw@gmail.com>
2020-11-26 11:09:59 +0000

    Objects introduction

--- Module-3/scripts/Task-3.4.3.js
+++ Module-3/scripts/Task-3.4.3.js
@@ -3,5 +3,5 @@ var myFavoriteFood = ['Kebab', 'Apples', 'Grapes','Pizza','Ice cream'];
 
 //Using for loops to run through the array.
 for (var index = 0; index <= myFavoriteFood.length; index++){
-    document.write(myFavoriteFood[index]);
+    document.write('<h1>' + myFavoriteFood[index] + '</h1>' +'<br>');
 }
---
+++ Module-3/scripts/objects.js
@@ -0,0 +1,11 @@
+var user = {
+    name: 'Madelaine',  //or use fullName
+    'full name': 'Madelaine',
+    age: 35,
+    height: '5ft 6'
+
+}
+
+user.name
+
+user["full name"]
