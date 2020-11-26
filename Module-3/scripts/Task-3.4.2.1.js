5709058c45f00f9e91b3775b858fad5f75245903
Xavianna <madelainelauw@gmail.com>
2020-11-26 10:41:22 +0000

    Arrays + For Loops

---
+++ Module-3/scripts/Task-3.4.2.1.js
@@ -0,0 +1,12 @@
+// var myFavoriteThing = ['Broccoli', 1024, 'Sherlock'];
+
+// myFavoriteThing.push('Dancing');
+
+
+var myFavoriteFood = ['Kebab', 'Apples', 'Grapes','Pizza','Ice cream'];
+
+
+//Using for loops to run through the array.
+for (var index = 0; index <= myFavoriteFood.length; index++){
+    console.log(myFavoriteFood[index]);
+}
