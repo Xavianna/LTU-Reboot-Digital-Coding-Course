053f734286e3a2dbb0a95fb0f80a081b2146d99c
Xavianna <madelainelauw@gmail.com>
2020-11-26 10:46:33 +0000

    Update file names

---
+++ Module-3/scripts/Task-3.4.3.js
@@ -0,0 +1,7 @@
+var myFavoriteFood = ['Kebab', 'Apples', 'Grapes','Pizza','Ice cream'];
+
+
+//Using for loops to run through the array.
+for (var index = 0; index <= myFavoriteFood.length; index++){
+    document.write(myFavoriteFood[index]);
+}
