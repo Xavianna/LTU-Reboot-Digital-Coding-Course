5b046a8a7e1085ee1ed000c35aab9db5bb933afe
Xavianna <madelainelauw@gmail.com>
2020-11-27 11:08:39 +0000

    Task 3.6.1

---
+++ Module-3/scripts/Task-3.6.1.js
@@ -0,0 +1,11 @@
+// var pageBody = document.getElementByTagName('body')[0]
+
+// pageBody.style.backgroundColor = 'red';
+
+// pageBody.style.paddingTop = '10px';
+
+let anchorTag = document.querySelector('a');
+    anchorTag.setAttribute('href', 'www.google.com');
+
+let paragraph = document.querySelectorAll('p')[0];
+    paragraph.setAttribute('')
