df0cb46bbb908e99d2de11c1b5ae16bcde6974b1
Xavianna <madelainelauw@gmail.com>
2020-11-27 13:19:16 +0000

    Added prevent default element.

--- Module-3/scripts/Task-3.6.2.js
+++ Module-3/scripts/Task-3.6.2.js
@@ -7,3 +7,12 @@ let paragraphText = document.createTextNode('I am a paragraph created in JavaScr
     paragraph.appendChild ( paragraphText );
 
 div.appendChild( paragraph );
+
+let link = document.querySelector('a');
+
+// console.log ( link );
+
+link.addEventListener ( 'click', function( event ){
+    event.preventDefault();
+    console.log('Default disabled');
+}); //Preventing people from going to the link
